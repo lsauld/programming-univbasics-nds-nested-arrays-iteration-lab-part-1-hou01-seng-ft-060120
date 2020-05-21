6e51c87384fe696a9366892f2ec5e97fa4706877
leonie crutchfield <leoniecrutchfield@gmail.com>
2020-05-21 23:25:41 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,7 @@ def find_even_values(src)
   
   element_index = 0 
   
-  while [element_index] < src[row_index].count do
+  while element_index<< src[row_index].count do
     
       if src[row_index]
     
